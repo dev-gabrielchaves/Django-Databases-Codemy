4d8d2aa221da0caee61bad5e3d88561a3b332f32
dev-gabrielchaves <dev.gchaves@gmail.com>
2023-10-08 10:08:13 -0300

    Class #3 - Pulling Data From The Database

--- Django_DB/website/views.py
+++ Django_DB/website/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Member
 
 def home(request):
-    return render(request, 'home.html')
+#   This is will sign everything that is in our database into this variable...
+    all_members = Member.objects.all
+    return render(request, 'home.html', {'all' : all_members})

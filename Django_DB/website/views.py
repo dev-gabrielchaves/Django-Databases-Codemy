82116d7e683dddab9058d90275c4556b9caa7f49
dev-gabrielchaves <dev.gchaves@gmail.com>
2023-10-08 11:37:09 -0300

    Class #4 - Creating Web Forms

--- Django_DB/website/views.py
+++ Django_DB/website/views.py
@@ -5,3 +5,7 @@ def home(request):
 #   This is will sign everything that is in our database into this variable...
     all_members = Member.objects.all
     return render(request, 'home.html', {'all' : all_members})
+
+# Creating join, we will integrate our web page with our database here...
+def join(request):
+    return render(request, 'join.html', {})
